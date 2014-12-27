9d79b812e1a4d977e649b3afcfaa11338ef5b511
Paradoxon <two4god@gmail.com>
2014-12-27 12:00:46 +0000

    added constants.h wich contains all path and other constants

---
+++ constants.h
@@ -0,0 +1,10 @@
+#ifndef __BAUERNHOFCONSTANTS_H__
+#define __BAUERNHOFCONSTANTS_H__
+
+#define ART_DIR_MISSING "Ich konnte das Art Verzeichnis leider nicht finden :( schau bitte noch mal nach ob alles richtig installiert wurde."
+#define ART_DIR "art"
+#define WORLDS_DIR "worlds"
+#define BASE_PATH "Bauernhof"
+#define PREFERENCES_FILE "Bauernhof"
+
+#endif
